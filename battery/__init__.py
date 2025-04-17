c4017e561c61d2ef6b998df1bab0f143e343ecc9
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2025-04-17 00:56:45 -0700

    Load voltage_curves.json

---
+++ battery/__init__.py
@@ -0,0 +1,9 @@
+import json
+
+# NOTE: I'm pretty sure that the current working directory
+# will always be at the root level so it should be okay to
+# do this.
+with open('lib/battery/voltage_curves.json') as file:
+    voltage_curves = json.load(file)
+    for battery_type in voltage_curves:
+        voltage_curves[battery_type] = list(reversed(voltage_curves[battery_type]))
